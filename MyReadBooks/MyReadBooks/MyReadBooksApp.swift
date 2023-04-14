16fbfa47601d1d26b0b807eb33aa2c9204879e1a
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2023-04-14 12:31:08 +0300

    Create project

---
+++ MyReadBooks/MyReadBooks/MyReadBooksApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyReadBooksApp.swift
+//  MyReadBooks
+//
+//  Created by Ваня Науменко on 14.04.23.
+//
+
+import SwiftUI
+
+@main
+struct MyReadBooksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
